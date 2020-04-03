59d9fe085418e5a843144337c6be0fb5cfcfbd67
luckyxp <2271613696@qq.com>
2020-04-04 01:43:57 +0800

    over

--- src/main/java/com/xp/controller/StudentController.java
+++ src/main/java/com/xp/controller/StudentController.java
@@ -8,8 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -38,5 +37,33 @@ public class StudentController {
         Collection<Grade> grades = gradeDao.getGrades();
         modelMap.addAttribute("grades", grades);
         return "forward:/page/student/add";
+    }
+    @PostMapping("/student")
+    public String addStudent(Student student) {
+        studentDao.save(student);
+        return "redirect:/students";
+    }
+    //来到修改页面
+    @GetMapping("/student/{id}")
+    public String toEditPage(@PathVariable("id") Integer id,ModelMap modelMap) {
+        //查出学生信息,在各个input框内显示
+        Student student = studentDao.get(id);
+        modelMap.addAttribute("student", student);
+        //查出所有年级,在页面显示
+        Collection<Grade> grades = gradeDao.getGrades();
+        modelMap.addAttribute("grades", grades);
+        return "forward:/page/student/add";
+    }
+    //修改学生信息
+    @PutMapping("/student")
+    public String updateStudent(Student student) {
+        studentDao.save(student);
+        return "redirect:/students";
+    }
+    //删除学生信息
+    @DeleteMapping("/student/{id}")
+    public String deleteStudent(@PathVariable("id") Integer id) {
+        studentDao.delete(id);
+        return "redirect:/students";
     }
 }
--- src/main/java/com/xp/pojo/Student.java
+++ src/main/java/com/xp/pojo/Student.java
@@ -1,5 +1,7 @@
 package com.xp.pojo;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
 public class Student {
@@ -11,6 +13,7 @@ public class Student {
     //1 male, 0 female
     private Integer gender;
     private Grade grade;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birth;
 
     public Integer getId() {
